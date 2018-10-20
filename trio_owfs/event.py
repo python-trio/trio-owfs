9368208e4cd0958f8817bb521e95c5f4363e186d
Matthias Urlichs <matthias@urlichs.de>
2018-10-20 16:58:49 +0200

    Move event attributes to superclasses

--- trio_owfs/event.py
+++ trio_owfs/event.py
@@ -10,44 +10,46 @@ class Event:
     pass
 
 
+@attr.s
 class ServerEvent(Event):
     """Base class for all server-related events"""
-    pass
+    server = attr.ib()
 
 
 @attr.s
 class ServerRegistered(ServerEvent):
     """A new known server appears. The server is not yet connected!"""
-    server = attr.ib()
+    pass
 
 
 @attr.s
 class ServerConnected(ServerEvent):
     """We have connected to a server"""
-    server = attr.ib()
+    pass
 
 
 @attr.s
 class ServerDisconnected(ServerEvent):
     """We have disconnected from a server"""
-    server = attr.ib()
+    pass
 
 
 @attr.s
 class ServerDeregistered(ServerEvent):
     """This server is no longer known"""
-    server = attr.ib()
+    pass
 
 
+@attr.s
 class BusEvent(Event):
     """Base class for all Bus-related events"""
-    pass
+    bus = attr.ib()
 
 
 @attr.s
 class BusAdded(BusEvent):
     """The Bus has been created. Its location is not yet known!"""
-    bus = attr.ib()
+    pass
 
 
 class BusAdded_Path:
@@ -72,30 +74,31 @@ class BusAdded_Path:
 @attr.s
 class BusDeleted(BusEvent):
     """The Bus has been deleted"""
-    bus = attr.ib()
+    pass
 
 
+@attr.s
 class DeviceEvent(Event):
     """Base class for all device-related events"""
-    pass
+    device = attr.ib()
 
 
 @attr.s
 class DeviceAdded(DeviceEvent):
     """The device has been created. Its location is not yet known!"""
-    device = attr.ib()
+    pass
 
 
 @attr.s
 class DeviceDeleted(DeviceEvent):
     """The device has been deleted"""
-    device = attr.ib()
+    pass
 
 
 @attr.s
 class DeviceLocated(DeviceEvent):
     """The device has been found"""
-    device = attr.ib()
+    pass
 
     @property
     def server(self):
@@ -109,19 +112,18 @@ class DeviceLocated(DeviceEvent):
 @attr.s
 class DeviceNotFound(DeviceEvent):
     """The device's location is no longer known"""
-    device = attr.ib()
+    pass
 
 
 @attr.s
 class DeviceAlarm(DeviceEvent):
     """The device triggered an alarm condition."""
-    device = attr.ib()
+    pass
 
 
 @attr.s
 class DeviceValue(DeviceEvent):
     """The device poll has read a value."""
-    device = attr.ib()
     attribute = attr.ib()
     value = attr.ib()
 
