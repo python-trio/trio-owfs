e0d1d92d09885f9cec9a0a8199e996d398eeec86
Matthias Urlichs <matthias@urlichs.de>
2021-03-30 21:32:55 +0200

    Reinstate polling, dammit

--- asyncowfs/bus.py
+++ asyncowfs/bus.py
@@ -224,10 +224,10 @@ class Bus:
                 await p()
 
 
-#   def poll_temperature(self):
-#       """Read all temperature data"""
-#       return self._poll_simul("temperature", 1.2)
+    def poll_temperature(self):
+        """Read all temperature data"""
+        return self._poll_simul("temperature", 1.2)
 
-#   def poll_voltage(self):
-#       """Read all voltage data"""
-#       return self._poll_simul("voltage", 1.2)
+    def poll_voltage(self):
+        """Read all voltage data"""
+        return self._poll_simul("voltage", 1.2)
